56b4bbeea5a2b8aa3741e173ec03eb1f10611b6e
thomdotman <thom.dot@hotmail.com>
2017-03-08 09:53:54 +0100

    Main file importing the code blocks

---
+++ main.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 08 08:47:37 2017
+
+@author: thom
+"""
+
+####### BASE IMPORTS ########
+import numpy as np
+from math import *
+import matplotlib.pyplot as plt
+
+
+####### OWN PROGRAMS IMPORT #######
+
+
+####### BASE DATA #######
+
